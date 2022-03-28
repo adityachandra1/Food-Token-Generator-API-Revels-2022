5721206992b8c8f24b4472c3adbe3a616d0ffda2
Chintan0539 <chintanpatel0539@gmail.com>
2022-03-28 23:41:23 +0530

    basics done

---
+++ new_client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import GenTokens from "./components/GenTokens";
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/send-tokens" element={GenTokens} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
