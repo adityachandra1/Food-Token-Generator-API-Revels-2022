06228e76713e9c0c6db7d64391030f2848676760
Aditya Chandra <adityachandra9898@gmail.com>
2022-03-29 23:37:34 +0530

    added-info-routes

--- api/routes/infoRoutes.js
+++ api/routes/infoRoutes.js
@@ -1,2 +1,25 @@
 //get user
 //get cat
+
+const express = require("express");
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const Volunteer = require('../models/VolunteerModel');
+const Category = require('../models/categoryModel');
+
+
+router.get('/info-volunteer', async (req, res) => {
+    try {
+        const toBeRedeemed = req.query.token;
+        const payload = jwt.verify(toBeRedeemed, 'HFS');
+        const volun = await Volunteer.findOne({ 'email': payload.email });
+        const cat = await Category.findById(volun.category);
+        res.json({"name": volun.name, "email": volun.email, "role": volun.role,  "tokensIssued": volun.foodTokens.length, "category": cat.category});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.toString() });
+    }
+});
+
+module.exports = router;
