6c6968a7b0904e49f054255f2977f32b5ff50778
Chintan0539 <chintanpatel0539@gmail.com>
2022-03-30 01:36:31 +0530

    bit of gettokens

--- new_client/src/pages/GenTokens.jsx
+++ new_client/src/pages/GenTokens.jsx
@@ -1,51 +1,21 @@
 import React from "react";
-import { Table, Tag, Space } from 'antd';
+import "./CSS/GenTokens.css";
 
 const GenTokens = () => {
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-      btn: <button>Generate Tokens</button>,
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-      btn: <button>Generate Tokens</button>,
-    },
-  ];
-
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
-    },
-    {
-      title: "Generate Tokens",
-      dataIndex: "btn"
-    }
-  ];
-  
-
-  return (<div>
-    <h1>GenTokens</h1>
-    <Table dataSource={dataSource} columns={columns} />;
-  </div>);
+  return (
+    <div className="gentokens-page">
+      <div className="list-container">
+        {" "}
+        <h1>Welcome back, Parthiv</h1>
+        <p>System admin and web development</p>
+        <div className="tab-menu">
+          <button className="active">Organisers</button>
+          <button>Core Commitee</button>
+          <button>Volunteers</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GenTokens;
