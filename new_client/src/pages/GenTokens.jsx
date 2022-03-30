d658e6d79cd194abd9a71fff6a2aa6c5cb7555d8
Chintan0539 <chintanpatel0539@gmail.com>
2022-03-30 23:01:30 +0530

    sidebar added

--- new_client/src/pages/GenTokens.jsx
+++ new_client/src/pages/GenTokens.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import Sidebar from "../components/sidebar";
 import "./CSS/GenTokens.css";
 
 const GenTokens = () => {
   return (
-    <div className="gentokens-page">
-      <div className="list-container">
-        {" "}
-        <h1>Welcome back, Parthiv</h1>
-        <p>System admin and web development</p>
-        <div className="tab-menu">
-          <button className="active">Organisers</button>
-          <button>Core Commitee</button>
-          <button>Volunteers</button>
-        </div>
-        <div className="search-container">
-          <p>List of Organisers:-</p>
-          <button>Select all</button>
+    <div className="main-page">
+      <Sidebar />
+      <div className="gentokens-page">
+        <div className="list-container">
+          {" "}
+          <h1>Welcome back, Parthiv</h1>
+          <p>System admin and web development</p>
+          <div className="tab-menu">
+            <button className="active">Organisers</button>
+            <button>Core Commitee</button>
+            <button>Volunteers</button>
+          </div>
+          <div className="search-container">
+            <p>List of Organisers:-</p>
+            <button>Select all</button>
+          </div>
         </div>
       </div>
     </div>
