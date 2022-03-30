16d02ad9e4c6ac3579b7845ebffd00a8c2bc6f63
Yash Gulati <yashgul@gmail.com>
2022-03-30 23:02:46 +0530

    filter

--- new_client/src/pages/GenTokens.jsx
+++ new_client/src/pages/GenTokens.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+
 import "./CSS/GenTokens.css";
+import React, { useState } from "react";
+
 
 const GenTokens = () => {
+  const USERS = [
+    {  name: 'Andy', role: "Core Committee Member" },
+    {  name: 'Garvit',  role: "Core Committee Member"},
+    {  name: 'Yash', role: "Core Committee Member" },
+    {  name: 'ajdaijd', role: "Organizer" },
+    {  name: 'aaaaa', role: "Organizer" },
+    {  name: 'xxaaa', role:"Organizer" },
+    {  name: 'Andy', role: "Organizer" },
+    {  name: 'Garvit', role: "Organizer" },
+    {  name: 'Yash', role: "Volunteer" },
+    {  name: 'ajdaijd', role: "Volunteer" },
+    {  name: 'aaaaa', role: "Volunteer" },
+    {  name: 'xxaaa', role: "Volunteer" }
+  ];
+  
+  const [name, setName] = useState('');
+
+  // the search result
+  const [foundUsers, setFoundUsers] = useState(USERS);
+
+  const filter = (e) => {
+    const keyword = e.target.value;
+
+    if (keyword !== '') {
+      const results = USERS.filter((user) => {
+        return user.name.toLowerCase().startsWith(keyword.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setFoundUsers(results);
+    } else {
+      setFoundUsers(USERS);
+      // If the text field is empty, show all users
+    }
+
+    setName(keyword);
+  };
+
   return (
     <div className="gentokens-page">
       <div className="list-container">
@@ -13,11 +52,36 @@ const GenTokens = () => {
           <button>Core Commitee</button>
           <button>Volunteers</button>
         </div>
-        <div className="search-container">
-          <p>List of Organisers:-</p>
-          <button>Select all</button>
-        </div>
+       
       </div>
+
+
+      <input
+        type="search"
+        value={name}
+        onChange={filter}
+        className="input"
+        placeholder="Filter"
+      />
+      <div className="container">
+     
+
+      <div className="user-list">
+        {foundUsers && foundUsers.length > 0 ? (
+          foundUsers.map((user) => (
+            <li key={user.id} className="user">
+                 <span className="user-name">
+                   <input type="checkbox" id="user-id"  /></span>
+              <span className="user-name">{user.name}</span>
+              <span className="user-age">{user.role} </span>
+            </li>
+          ))
+        ) : (
+          <h1>No results found!</h1>
+        )}
+      </div>
+    </div>
+
     </div>
   );
 };
