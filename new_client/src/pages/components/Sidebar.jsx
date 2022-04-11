21e417aba1a04c3469381389112090196e3b86dc
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-04-12 00:33:59 +0530

    HFS responsive

--- new_client/src/pages/components/Sidebar.jsx
+++ new_client/src/pages/components/Sidebar.jsx
@@ -5,14 +5,23 @@ import "./css/Sidebar.css";
 function Sidebar() {
   const navigate = useNavigate();
   return (
-    <div className="sidebar-container d-flex flex-column">
-      <button className="active-sidebar-btn sidebar-btn" onClick={() => {
-        navigate("/dashboard");
-      }} >Dashboard</button>
-      <button className="sidebar-btn" onClick={() => {
-        navigate("/history");
-      }} >History</button>
-      
+    <div className="sidebar-container d-flex flex-column align-items-center">
+      <button
+        className="active-sidebar-btn sidebar-btn"
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        Dashboard
+      </button>
+      <button
+        className="sidebar-btn"
+        onClick={() => {
+          navigate("/history");
+        }}
+      >
+        History
+      </button>
     </div>
   );
 }
